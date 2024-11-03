e6b8ad9f4dfceaa02864507d3a2c7840953c3069
ysygMhdyt <shenyiyi2001@163.com>
2024-11-03 00:23:07 -0400

    datepicker

--- src/app/more_page/page.tsx
+++ src/app/more_page/page.tsx
@@ -5,7 +5,11 @@ import SmallCard from '../../components/card/SmallCard';
 import { useRouter } from 'next/navigation';
 import Button from '../../components/button/button';
 import { useState } from 'react';
-// import { DatePicker } from 'antd';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+
 
 export default function MoreCards() {
     const router = useRouter();
@@ -15,19 +19,19 @@ export default function MoreCards() {
         router.push('/new_page');
     };
 
-    // const onChange = (date, dateString) => {
-    //   console.log(date, dateString);
-    // };
+    const [selectedDate, setSelectedDate] = useState(null);
 
     return (
-        <div className="min-h-screen bg-gradient-to-b flex flex-col items-center pt-16 px-4">
+        <div className="min-h-screen bg-gradient-to-b flex flex-col items-center pt-12 px-4">
     
           {/* 标题 */}
-          <h1 className="text-3xl font-bold text-gray-200 mb-10">Explore More</h1>
+          <h1 className="text-3xl font-bold text-gray-200 mb-8">Explore More</h1>
     
-          {/* <DatePicker onChange={onChange} /> */}
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker defaultValue={dayjs('2024-11-05')} className="w-full bg-white rounded"/>
+          </LocalizationProvider>
           {/* 卡片网格，flex-grow 保持卡片区域填充剩余空间 */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 w-full max-w-md sm:max-w-lg">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 w-full max-w-md sm:max-w-lg mt-4">
             <SmallCard title="Card 1" text="Some additional content." imageSrc="/image/logDaily.jpeg" />
             <SmallCard title="Card 2" text="Some additional content." imageSrc="/image/logDaily.jpeg" />
             <SmallCard title="Card 3" text="Some additional content." imageSrc="/image/logDaily.jpeg" />
@@ -36,7 +40,7 @@ export default function MoreCards() {
           </div>
     
           {/* 按钮 */}
-          <div className="mt-8 mb-4">
+          <div className="mt-4 mb-2">
             <Button 
               label="Back" 
               onClick={handleBack} 
@@ -45,7 +49,7 @@ export default function MoreCards() {
           </div>
     
           {/* 搜索框 */}
-          <div className="p-4 w-full">
+          <div className="p-1 w-full">
             <input
               type="text"
               value={searchTerm}
